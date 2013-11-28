b8a624e4cf3bba9ace5514fc4cafc35d105e486b
Michael Mancuso <tarkenfire@fullsail.edu>
2013-11-28 17:14:05 -0500

    Initial commits.

---
+++ android-projects/circe/src/com/hinodesoftworks/circe/ServerList.java
@@ -0,0 +1,25 @@
+package com.hinodesoftworks.circe;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ServerList extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_server_list);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.server_list, menu);
+		return true;
+	}
+
+}
